0c6d65a7c8c6cd545a5599e5eae6ce7c801bb9ba
Victor <beeproger@yandex.ru>
2024-03-01 00:54:48 +1000

    pereputochka

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -12,10 +12,10 @@ export default function library() {
         <Link href="/viewer/1">
                 <div className="carousel-item bg-green-500 w-100 h-80 rounded-box centered-text text-5xl font-bold">
                     <div className="flex flex-col">
-                        <div>Собачье сердце</div>
+                        <div>Джерри островитянин</div>
                         <div className="navbar text-2xl">
                             <div className="navbar-start">
-                                Автор: Михаил Булгаков
+                                Автор: Джек Лондон
                             </div>
                         </div>
                     </div>
@@ -25,10 +25,10 @@ export default function library() {
             <Link href="/viewer/2">
                 <div className="carousel-item bg-green-500 w-100 h-80 rounded-box centered-text text-5xl font-bold">
                     <div className="flex flex-col">
-                        <div>Джерри островитянин</div>
+                        <div>Собачье сердце</div>
                         <div className="navbar text-2xl">
                             <div className="navbar-start">
-                                Автор: Джек Лондон
+                                Автор: Михаил Булгаков
                             </div>
                         </div>
                     </div>
